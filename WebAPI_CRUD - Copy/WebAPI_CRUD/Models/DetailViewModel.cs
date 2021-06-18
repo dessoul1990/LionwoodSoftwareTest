a2f9840f15ba3f35192fcec1075a25901cbf9461
Roman Okipnyy <dessoul1990@gmail.com>
2021-06-18 17:43:27 +0300

    task

---
+++ WebAPI_CRUD - Copy/WebAPI_CRUD/Models/DetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_CRUD.Models
+{
+    public class DetailViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Country { get; set; }
+
+    }
+}
